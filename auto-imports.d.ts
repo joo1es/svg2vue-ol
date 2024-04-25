b37de3eb0f6646e8ae2730f6e36441564bbc8b75
陈涛 <jooies@qq.com>
2024-04-25 14:24:35 +0800

    chore: update deps

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -52,6 +52,7 @@ declare global {
   const onUnmounted: typeof import('vue')['onUnmounted']
   const onUpdated: typeof import('vue')['onUpdated']
   const provide: typeof import('vue')['provide']
+  const qRequest: typeof import('@oasis-end/request')['qRequest']
   const reactive: typeof import('vue')['reactive']
   const readonly: typeof import('vue')['readonly']
   const ref: typeof import('vue')['ref']
